bab26384abb99847a1db2643ac2b8f3ab03694df
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2023-11-08 17:10:53 +0600

    database connected

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const dbConnect = async() => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Connected connected succefully ${conn.connection.host}`)
+    console.log(`Database Connected Succefully ${conn.connection.host}`)
   } catch (error) {
     console.log(`error : ${error.message}`)
   }
